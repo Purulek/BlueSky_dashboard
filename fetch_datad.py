2037e604c73bf55c094e635941174c17664924f0
purulek <adam.purul@gmail.com>
2024-12-11 23:27:05 +0100

    next commit

--- fetch_datad.py
+++ fetch_datad.py
@@ -1,1 +1,11 @@
-from atproto import Client
+from atproto import Client, client_utils
+
+
+def main():
+    client = Client()
+    profile = client.login('my-handle', 'my-password')
+    print('Welcome,', profile.display_name)
+
+    text = client_utils.TextBuilder().text('Hello World from ').link('Python SDK', 'https://atproto.blue')
+    post = client.send_post(text)
+    client.like(post.uri, post.cid)
